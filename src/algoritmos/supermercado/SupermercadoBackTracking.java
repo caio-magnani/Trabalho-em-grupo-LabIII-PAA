af6cdf12f4051c6442dd63e7683ccd494944dc3d
Carlos Germano <germano.carlos2712@gmail.com>
2020-11-30 22:32:39 -0300

    Finalização do backtracking

--- src/algoritmos/supermercado/SupermercadoBackTracking.java
+++ src/algoritmos/supermercado/SupermercadoBackTracking.java
@@ -36,21 +36,28 @@ public class SupermercadoBackTracking implements Algoritmo<Produto> {
         LinkedList<Produto>melhorCombinacao=new LinkedList<Produto>();
         LinkedList<Produto>combinacaoAtual=new LinkedList<Produto>();
         Double melhorSoma=0D;
+        int melhorPeso=0;
         int i=0;//em qual indice do array esta
         int j=0;//em qual opção esta
         combinacaoAtual.add(productList.get(0));
         do{
             int pesoAtual=combinacaoAtual.stream().mapToInt(p->p.peso).sum();
-            if(pesoMax < pesoAtual ){
+            double precoAtual=combinacaoAtual.stream().mapToDouble(p->p.preco).sum();
+            if(pesoMax < pesoAtual || orcamentoMax < precoAtual){
                 i--;
                 j++;
                 combinacaoAtual.remove();
             }
             else{
-                Double somaAtual= combinacaoAtual.stream().mapToDouble(p->p.preco).sum();
+                Double somaAtual = combinacaoAtual.stream().mapToDouble(p->p.preco).sum();
+                int somaPrAtual = combinacaoAtual.stream().mapToInt(p->p.peso).sum();
                 if(melhorSoma<somaAtual) {
                     melhorCombinacao = (LinkedList<Produto>) combinacaoAtual.clone();
                     melhorSoma = somaAtual;
+                }
+                if(melhorPeso<somaPrAtual){
+                    melhorCombinacao = (LinkedList<Produto>) combinacaoAtual.clone();
+                    melhorPeso = somaPrAtual;
                 }
                 if(combinacaoAtual.size()>n) {
                     i--;
